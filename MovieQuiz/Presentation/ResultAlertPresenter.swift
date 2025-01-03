388e6ac6e358e48ed23d02746a59eb45c8fd0b6b
Fortovaya <wilov@yandex.ru>
2025-01-03 23:16:04 +0300

    Добавлена работа с сетью и реализован алерт протокола Error

--- MovieQuiz/Presentation/ResultAlertPresenter.swift
+++ MovieQuiz/Presentation/ResultAlertPresenter.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ResultAlertPresenter: AlertPresenterProtocol {
+final class ResultAlertPresenter: AlertPresenterProtocol {
     private weak var delegate: UIViewController?
     
     init(delegate: UIViewController?) {
