e49b6bc76c591662ab008e0ba275a16098449f52
Fortovaya <149900355+Fortovaya@users.noreply.github.com>
2025-01-12 14:43:59 +0300

    Вынесен метод showNextQuestionOrResults из MoveQuizViewController в Presenter

--- MovieQuiz/Presentation/MovieQuizPresenter.swift
+++ MovieQuiz/Presentation/MovieQuizPresenter.swift
@@ -13,6 +13,11 @@ final class MovieQuizPresenter {
     var currentQuestion: QuizQuestion?
     
     weak var viewController: MovieQuizViewController?
+    var questionFactory: QuestionFactoryProtocol?
+    private var statisticService: StatisticServiceProtocol = StatisticServiceImplementation()
+    
+    var correctAnswers: Int = .zero
+    
     
     func convert(model: QuizQuestion) -> QuizStepViewModel {
         return QuizStepViewModel(
@@ -34,18 +39,73 @@ final class MovieQuizPresenter {
     }
     
     func yesButtonClicked() {
-        guard let currentQuestion = currentQuestion else { return }
-        let givenAnswer = true
+        didAnswer(isYes: true)
+    }
+    
+    func noButtonClicked() {
+        didAnswer(isYes: false)
+    }
+    
+    private func didAnswer(isYes: Bool) {
+        guard let currentQuestion = currentQuestion else {
+            return
+        }
+        
+        let givenAnswer = isYes
         
         viewController?.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     
-    func noButtonClicked() {
-        guard let currentQuestion = currentQuestion else { return }
-        let givenAnswer = false
+    //MARK: - QuestionFactoryDelegate
+    func didReceiveNextQuestion(question: QuizQuestion?){
+        guard let question = question else { return }
         
-        viewController?.showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
+        currentQuestion = question
+        let viewModel = convert(model: question)
+        
+        DispatchQueue.main.async { [weak self] in
+            guard let self = self else { return }
+            self.viewController?.show(quiz: viewModel)
+        }
     }
+    
+    func showNextQuestionOrResults() {
+        if self.isLastQuestion() {
+            
+            statisticService.store(correct: correctAnswers, total:self.questionsAmount)
+            
+            let message = getGamesStatistic(correct: correctAnswers, total: self.questionsAmount)
+            let alertModel = AlertModel(title: "Этот раунд окончен",
+                                        message: message,
+                                        buttonText: "Сыграть еще раз",
+                                        completion: { [weak self] in
+                self?.resetQuestionIndex()
+                self?.correctAnswers = .zero
+                guard (self?.questionFactory) != nil else { return }
+                self?.questionFactory?.requestNextQuestion()
+            }
+            )
+            
+            guard let alertPresenter = viewController?.alertPresenter else { return }
+            alertPresenter.showAlert(with: alertModel)
+        } else {
+            self.switchToNextQuestion()
+            guard let questionFactory = questionFactory else { return }
+            questionFactory.requestNextQuestion()
+        }
+    }
+    
+    func getGamesStatistic(correct count: Int, total amount: Int) -> String {
+//        guard let statisticService = statisticService else { return "Ваш результат: \(count)/\(amount)"}
+        
+        let score = "Ваш результат: \(count)/\(amount)"
+        let gamesCount = "Количество сыгранных квизов: \(statisticService.gamesCount)"
+        let record = "Рекорд: \(statisticService.bestGame.correct)/\(statisticService.bestGame.total) (\(statisticService.bestGame.date.dateTimeString))"
+        let totalAccuracy = "Средняя точность: \(String(format: "%.2f", statisticService.totalAccuracy))%"
+        
+        return [score, gamesCount, record, totalAccuracy].joined(separator: "\n")
+    }
+
     
 }
 
