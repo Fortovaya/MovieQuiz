b9c0ea3424fac5576c6b71b59d5ef519adcff1e1
Fortovaya <wilov@yandex.ru>
2025-01-12 23:48:08 +0300

    Реализованы UI-тесты

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-final class MovieQuizViewController: UIViewController {
+final class MovieQuizViewController: UIViewController, MovieQuizViewControllerProtocol {
     //MARK: - Variables
     @IBOutlet private weak var counterLabel: UILabel!
     @IBOutlet private weak var imageView: UIImageView!
@@ -10,7 +10,7 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private weak var activityIndicator: UIActivityIndicatorView!
     
     private var presenter: MovieQuizPresenter!
-
+    
     var alertPresenter: ResultAlertPresenter?
     private var statisticService: StatisticServiceProtocol?
     
@@ -26,6 +26,8 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderWidth = 8
         imageView.layer.cornerRadius = 20
         
+        resetImageBorder()
+        
         alertPresenter = ResultAlertPresenter(viewController: self)
         statisticService = StatisticServiceImplementation()
         
@@ -36,10 +38,10 @@ final class MovieQuizViewController: UIViewController {
     }
     
     // MARK: - Стиль статус-бара
-       override var preferredStatusBarStyle: UIStatusBarStyle {
-           return .lightContent
-       }
-
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     //MARK: - @IBAction
     @IBAction private func yesButtonClicked(_ sender: UIButton) {
         presenter.yesButtonClicked()
@@ -50,78 +52,50 @@ final class MovieQuizViewController: UIViewController {
     }
     
     //MARK: - private func
-    
     func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
-
     
-//    func show(quiz result: QuizResultsViewModel) {
-//            imageView.image = result.image
-//            textLabel.text = result.question
-//            counterLabel.text = result.questionNumber
-//            
-//            let currentGameResultLine = "Ваш результат: \(presenter.correctAnswers)/\(presenter.questionsAmount)"
-//            // Отображаем результат игры
-//            
-//            if let statisticService = statisticService {
-//                statisticService.store(correct: presenter.correctAnswers, total: presenter.questionsAmount)
-//            }
-//            
-//            let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
-//                self?.presenter.restartGame()
-//                self?.questionFactory?.loadData()
-//            }
-//        }
-    
-    
-    func showAnswerResult(isCorrect: Bool) {
-        presenter.didAnswer(isCorrectAnswer: isCorrect)
-        
-        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
-            guard let self = self else { return }
-            self.presenter.showNextQuestionOrResults()
-            self.imageView.layer.borderColor = UIColor.clear.cgColor
-        }
+    func highlightImageBorder(isCorrectAnswer: Bool) {
+        imageView.layer.borderColor = isCorrectAnswer ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
     }
     
-    private func showNextQuestionOrResults() {
-            if presenter.isLastQuestion() {
-                let text = presenter.getGamesStatistic(correct: presenter.correctAnswers, total: presenter.questionsAmount)
-                // Отобразить информацию о завершении игры
-            }
-        }
+    func resetImageBorder() {
+        imageView.layer.borderColor = UIColor.clear.cgColor
+    }
     
-    private func blockButton(isEnabled: Bool) {
+    func blockButton(isEnabled: Bool) {
         noButton.isEnabled = isEnabled
         yesButton.isEnabled = isEnabled
         noButton.alpha = isEnabled ? 1.0 : 0.5
         yesButton.alpha = isEnabled ? 1.0 : 0.5
     }
     
-    // отображение индикатора
     func showLoadingIndicator(){
         activityIndicator.startAnimating()
     }
-    //скрытие индикатора
+    
     func hideLoadingIndicator(){
         activityIndicator.stopAnimating()
     }
     
     func showNetworkError(message: String) {
         hideLoadingIndicator()
-        let model = AlertModel(title: "Ошибка",
-                               message: message,
-                               buttonText: "Попробовать еще раз") { [weak self] in
+        
+        let alert = UIAlertController(
+            title: "Ошибка",
+            message: message,
+            preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Попробовать ещё раз", style: .default) { [weak self] _ in
             guard let self = self else { return }
             self.presenter.restartGame()
         }
         
-        alertPresenter?.showAlert(with: model)
+        alert.addAction(action)
+        self.present(alert, animated: true, completion: nil)
     }
 }
 
---
+++ MovieQuiz/Services/MovieQuizViewControllerProtocol.swift
@@ -0,0 +1,17 @@
+//
+//  MovieQuizViewControllerProtocol.swift
+//  MovieQuiz
+//
+//  Created by Алина on 12.01.2025.
+//
+
+protocol MovieQuizViewControllerProtocol: AnyObject {
+    
+    func show(quiz step: QuizStepViewModel)
+    func highlightImageBorder(isCorrectAnswer: Bool)
+    func showLoadingIndicator()
+    func hideLoadingIndicator()
+    func showNetworkError(message: String)
+    func resetImageBorder()
+    func blockButton(isEnabled: Bool)
+}
